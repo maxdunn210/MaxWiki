7039709d29554b9912f489d1bd646b6394003400
Max Dunn <max@maxdunn.com>
2012-08-23 19:05:26 -0700

    Update Bichon site

--- public/themes/pawprint/theme_environment.rb
+++ public/themes/pawprint/theme_environment.rb
@@ -3,6 +3,6 @@ MY_CONFIG[:hide_empty_left_column] = false
 MY_CONFIG[:hide_byline_on_page] = false
 MY_CONFIG[:hide_breadcrumb] = false
 MY_CONFIG[:footer_edit_on_right] = true
-MY_CONFIG[:hide_menu] = true
-MY_CONFIG[:menu_on_left] = true
+MY_CONFIG[:hide_menu] = false
+MY_CONFIG[:menu_on_left] = false
 
